41678550aac7e6803fe3a4c601e9d399489162b0
후생 <gntodtndls156@gmail.com>
2020-09-23 21:20:40 +0900

    03주차 실습

---
+++ 03주차실습/ExerciseCh1_5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void Delegate();
+
+namespace ExerciseCh1_5_1
+{
+    class Program
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("In the Method 1....");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("In the Method 2....");
+        }
+        static void Main(string[] args)
+        {
+            Delegate d = new Delegate(Method2);
+            d += new Delegate(Method2);
+            d();
+        }
+    }
+}
---
+++ 03주차실습/ExerciseCh1_5_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCh1_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 0, k;
+            try
+            {
+                Console.WriteLine("Exception Checking...");
+                k = i / j;
+                Console.WriteLine("After Exception happening...");
+            } catch (Exception e)
+            {
+                Console.WriteLine("Caught Exception ");
+            } finally
+            {
+                Console.WriteLine("In the finally state");
+            }
+        }
+    }
+}
