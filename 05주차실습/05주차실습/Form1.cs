fd4fc33e899c7e0e6d72a729632972be99f1b103
gntodtndls156 <gntodtndls156@gmail.com>
2020-10-03 11:48:00 +0900

    05주차실습 1

---
+++ 05주차실습/05주차실습/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05주차실습
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        { // 왼쪽 버튼
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        { // 가운데 버튼
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        { // 오른쪽 버튼
+
+        }
+
+        // ---------------------
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        { // 굵게 버튼
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        { // 밑줄 버튼
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        { // 이탤릭 버튼
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        { // 취소선
+
+        }
+    }
+}
