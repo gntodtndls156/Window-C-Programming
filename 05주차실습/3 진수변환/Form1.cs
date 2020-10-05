67e4f8548f45cfcf24f8d544c132adf58ad21d96
gntodtndls156 <gntodtndls156@gmail.com>
2020-10-05 19:52:44 +0900

    3문제 추가

---
+++ 05주차실습/3 진수변환/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_진수변환
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int inputNumber;
+
+        private int SetNumber(int number) // inputNumber
+        {
+            return this.inputNumber = number;
+        }
+        private int GetNumber() //inputNumber
+        {
+            return this.inputNumber;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // 변환 버튼
+            
+            SetNumber(int.Parse(textBox1.Text));
+            TextboxUpdate();
+        }
+
+        private void TextboxUpdate()
+        {
+            int number = GetNumber();
+            int MOK, NMG, count = 0;
+            int[] arrnum = new int[50];
+            do 
+            {
+                MOK = number / 2;
+                NMG = number - MOK * 2;
+                number = MOK;
+                arrnum[count++] = NMG;
+                //textBox2.Text += NMG.ToString();
+            } while (MOK != 0);
+            textBox2.Clear();
+            for(int i = count - 1; i >= 0; i--)
+            {
+                textBox2.Text += arrnum[i].ToString();
+                
+            }
+        }
+    }
+}
