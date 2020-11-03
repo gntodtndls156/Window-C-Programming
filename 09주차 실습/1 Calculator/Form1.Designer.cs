78159fec96727c3689265e8dc9720f7570414909
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-04 00:30:30 +0900

    09주차 실습 1문제 해결

---
+++ 09주차 실습/1 Calculator/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace _1_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(36, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "28";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(24, 54);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(36, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "88";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.checkBox2.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(147, 23);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(36, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "16";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            this.checkBox3.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(147, 54);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(36, 16);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "19";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            this.checkBox4.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Min :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 105);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 16);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Min";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(147, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 16);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Max";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 182);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ 09주차 실습/1 Calculator/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool[] boolarr = new bool[4] { false, false, false, false };
+        readonly int[] intarr = new int[4] { 28, 88, 16, 19 };
+
+        int MIN, MAX;
+
+        void init(bool state)
+        {
+            if(state)
+            this.MIN = 99;
+            else
+            this.MAX = 0;
+        }
+
+        void StateChecked()
+        {
+            if (radioButton1.Checked)
+            {
+                label1.Text = "Min :";
+                if (MIN == 99) MIN = 0;
+                label2.Text = MIN + "";
+            }
+            else if (radioButton2.Checked)
+            {
+                label1.Text = "Max :";
+                label2.Text = MAX + "";
+            }
+        }
+        void FunMin ()
+        {
+            init(true);
+            for(int i = 0; i <= 3; i++)
+            {
+                if(boolarr[i] && MIN > intarr[i])
+                {
+                    MIN = intarr[i];
+                }
+            }
+            StateChecked();
+        }
+        void FunMax()
+        {
+            init(false);
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("완료");
+                if (boolarr[i] && MAX < intarr[i])
+                {
+                    
+                    MAX = intarr[i];
+                }
+            }
+            StateChecked();
+        }
+
+
+        private bool ChangeBool(int number)
+        {
+            return boolarr[number] = !boolarr[number];
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        { // 28
+            if (checkBox1.Checked)
+                ChangeBool(0);
+            else ChangeBool(0);
+
+            FunMax();
+            FunMin();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        { // 16
+            if (checkBox3.Checked)
+                ChangeBool(2);
+            else ChangeBool(2);
+
+            FunMax();
+            FunMin();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        { // 88
+            if (checkBox2.Checked)
+                ChangeBool(1);
+            else ChangeBool(1);
+
+            FunMax();
+            FunMin();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        { // 19
+            if (checkBox4.Checked)
+                ChangeBool(3);
+            else ChangeBool(3);
+
+            FunMax();
+            FunMin();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            StateChecked();
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
