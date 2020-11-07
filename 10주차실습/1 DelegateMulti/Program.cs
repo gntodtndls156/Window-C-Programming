fed8ec61da3e502cead110ab76b35751ebbb2e58
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-08 00:33:38 +0900

    문제 3 추가

--- 10주차실습/1 DelegateMulti/Program.cs
+++ 10주차실습/1 DelegateMulti/Program.cs
@@ -9,22 +9,38 @@ namespace _1_DelegateMulti
     class Program
     {
         // 델리게이트 CalcDelegate 타입은 2개의 정수를 전달하고, 반환값이 void인 메소드를 가리키는 타입을 정의
-        delegate int CalcDelegate(int x, int y);
+        delegate void CalcDelegate(int x, int y);
 
-        static void Plus(int x, int y) { Console.WriteLine("x + y ==>" + (x + y)); }
-        static void Minus(int x, int y) { Console.WriteLine("x - y ==>" + (x - y)); }
-        static void Multiple(int x, int y) { Console.WriteLine("x * y ==>" + (x * y)); }
-        static void Divide(int x, int y) { Console.WriteLine("x / y ==>" + (x / y)); }
+        void Plus(int x, int y) { Console.WriteLine("x + y ==>" + (x + y)); }
+        void Minus(int x, int y) { Console.WriteLine("x - y ==>" + (x - y)); }
+        void Multiple(int x, int y) { Console.WriteLine("x * y ==>" + (x * y)); }
+        void Divide(int x, int y) { Console.WriteLine("x / y ==>" + (x / y)); }
 
         static void Main(string[] args)
         {
-            CalcDelegate del1 = new CalcDelegate(Plus);
-            int result = del1(20, 10); // Plus() 호출
-            Console.WriteLine("result = " + result);
+            Program program = new Program();
+            // CalcDelegate 타입의 객체를 선언하고, 각각의 메소드를 델리게이트의 간단한 사용형식으로 연결
+            CalcDelegate del1 = program.Plus;
+            CalcDelegate del2 = program.Minus;
+            CalcDelegate del3 = program.Multiple;
+            CalcDelegate del4 = program.Divide;
 
-            CalcDelegate del2 = new CalcDelegate(Minus);
-            result = del2(20, 10); // Minus() 호출
-            Console.WriteLine("result = " + result);
+            // 델리게이트 객체 del1에 del2, del3, del4를 차례로 추가
+            del1 += del2;
+            del1 += del3;
+            del1 += del4;
+
+            // 델리게이트 del1 객체에 (20, 10)을 넘겨주면
+            // 누적 추가된 del2, del3, del4 객체에 동일하게 적용되어 각각 연결된 메소드를 호출
+            del1(20, 10);
+
+            // 누적된 del1 객체에서 del3, del4 객체를 차례로 삭제
+            del1 -= del3;
+            del1 -= del4;
+
+            // 델리게이트 del1 객체에 (20, 10)을 넘겨주면
+            // 누적 추가된 del3, del4 객체는 삭제되어 del1과 del2 객체만 적용
+            del1(20, 10);
 
         }
     }
--- 10주차실습/EventApp/Program.cs
+++ 10주차실습/EventApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,29 @@ namespace EventApp
 {
     class Program
     {
+        // 델리게이트 ButtonEvent 타입을 선언
+        public delegate void ButtonEvent();
+        //이벤트 델리게이트 ButtonEvent를 사용하여 선언
+        public static event ButtonEvent evtClick;
+
+        //onClick() 메소드 정의, 버튼 클릭 기능을 수행하기 위한 메소드
+        public static void onClick()
+        {
+            evtClick(); // 델리게이트 객체 evtClick를 통해 연결된 메소드를 호출
+        }
+
+        // MyHandler() 메소드 정의, 버튼 클릭 이벤트 발생시 연결한 이벤트 처리기
+        public static void MyHandler()
+        {
+            Console.WriteLine("버튼 클릭 처리기 수행");
+        }
+
         static void Main(string[] args)
         {
+            // 델리게이트 ButtonEvent 를 통해 이벤트 처리기 MyHandler를 evtClick이벤트에 등록
+            evtClick += new ButtonEvent(MyHandler);
+            onClick(); // 버튼을 실제로 클릭할 수 없었으므로 버튼이 클릭되었다고 가정하고 onClick() 메소드를 호출
+
         }
     }
 }
