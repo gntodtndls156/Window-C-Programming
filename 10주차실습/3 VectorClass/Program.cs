1cf548f56ba3444e491ad65e8c8097f38c56ad23
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-09 12:53:42 +0900

    실습 4문제 추가

--- 10주차실습/3 VectorClass/Program.cs
+++ 10주차실습/3 VectorClass/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace _3_VectorClass
 {
-    class Program
+    class Vector
     {
+        private int[] v;
+
+        public Vector(int size)
+        {
+            v = new int[size];
+        }
+        public int this[int index]
+        {
+            get { return v[index]; }
+            set { v[index] = value; }
+        }
+        
+        public static Vector operator ++(Vector vec)
+        {
+            for (int i = 0; i < vec.v.Length; i++)
+                vec[i]++;
+            return vec;
+        }
+        public static Vector operator --(Vector vec)
+        {
+            for (int i = 0; i < vec.v.Length; i++)
+                vec[i]--;
+            return vec;
+        }
+
         static void Main(string[] args)
         {
+            Vector vector = new Vector(5);
+
+            for (int i = 0; i < 5; i++)
+                vector[i] = 10 + i;
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(vector[i] + " ");
+            Console.WriteLine();
+
+            vector++;
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(vector[i] + " ");
+            Console.WriteLine();
+
+            vector--;
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(vector[i] + " ");
+            Console.WriteLine();
         }
     }
 }
