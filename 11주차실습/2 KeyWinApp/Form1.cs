3be818e8b65d72908cf8c36028e01e58664f7a34
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-16 17:40:16 +0900

    문자 2추가

--- 11주차실습/2 KeyWinApp/Form1.cs
+++ 11주차실습/2 KeyWinApp/Form1.cs
@@ -16,5 +16,25 @@ namespace _2_KeyWinApp
         {
             InitializeComponent();
         }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keys.F1 <= e.KeyCode || Keys.A <= e.KeyCode || Keys.D0 <= e.KeyCode || Keys.Escape <= e.KeyCode)
+            {
+                if(e.Control)
+                {
+                    MessageBox.Show(e.KeyCode.ToString() + "+ Ctrl", "누른 키");
+                } else if (e.Alt)
+                {
+                    MessageBox.Show(e.KeyCode.ToString() + "+ Alt", "누른 키");
+                } else if (e.Shift)
+                {
+                    MessageBox.Show(e.KeyCode.ToString() + "+ Shift", "누른 키");
+                } else
+                {
+                    MessageBox.Show(e.KeyCode.ToString(), "누른 키");
+                }
+            }
+        }
     }
 }
