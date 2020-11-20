0a06ba60ccea3b84e3c06434946e579ddee5d28e
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-20 21:42:00 +0900

    실습 1 추가

--- 12주차실습/1 Overriding/Program.cs
+++ 12주차실습/1 Overriding/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace _1_Overriding
 {
+	class ArmorSuite
+    {
+		public virtual void Initialize()
+        {
+			Console.WriteLine("Armored");
+        }
+    }
+	class IronMan : ArmorSuite
+    {
+		new public void Initialize()
+        {
+			base.Initialize();
+			Console.WriteLine("Repulsor Rays Armed");
+        }
+    }
+	class WarMachine : ArmorSuite 
+	{
+		new public void Initialize()
+		{
+			base.Initialize();
+			Console.WriteLine("Double-Barrel Cannons Armed");
+			Console.WriteLine("Micro-Rocket Launcher Armed");
+        }
+
+	} 
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Creating ArmorSuite...");
+			ArmorSuite armorsuite = new ArmorSuite();
+			armorsuite.Initialize();
+
+			Console.WriteLine("\nCreating IronMan...");
+			IronMan ironman = new IronMan();
+			ironman.Initialize();
+
+			Console.WriteLine("\nCreating WarMaching...");
+			WarMachine warMachine = new WarMachine();
+			warMachine.Initialize();
 		}
 	}
+
+	
 }
+
