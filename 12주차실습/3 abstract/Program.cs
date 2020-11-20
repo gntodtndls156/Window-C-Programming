de12431b06bbc6e59fc49ad30adfbd3600b20893
gntodtndls156 <gntodtndls156@gmail.com>
2020-11-20 21:42:18 +0900

    실습 3 추가

---
+++ 12주차실습/3 abstract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_abstract
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+
+    class Derived : AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived derived = new Derived();
+            derived.AbstractMethodA();
+            derived.PublicMethodA();
+
+        }
+    }
+}
